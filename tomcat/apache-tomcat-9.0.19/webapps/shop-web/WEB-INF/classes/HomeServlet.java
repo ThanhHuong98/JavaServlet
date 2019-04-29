f3e10bc8078cdcbf31d5bf5abe7766878103ea09
ntthanhhuong98 <ntthanhhuong98.fit@gmail.com>
2019-04-29 12:08:31 +0700

    [set up .jsp]

---
+++ tomcat/apache-tomcat-9.0.19/webapps/shop-web/WEB-INF/classes/HomeServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/webapp/shop-web/views/pages/home.jsp");
+        dispatcher.forward(request, response);
+
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
---
+++ tomcat/apache-tomcat-9.0.19/webapps/shop-web/views/partials/head.jsp
@@ -0,0 +1,24 @@
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="Content-Style-Type" content="text/css">
+	<html:base/>
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="img/fav.png">
+	<!-- Author Meta -->
+	<meta name="author" content="CodePixar">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>Karma Shop</title>
+	<!--
+		CSS
+		============================================= -->
+
+
+
+</head>
